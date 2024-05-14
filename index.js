9488ca5919911524eed21d53a3fa7c4c6d8230f0
kevokt <kevinoktavian08@gmail.com>
2024-05-14 14:28:46 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 3000;
+
+// Setup EJS
+app.set("view engine", "ejs");
+app.use(expressLayouts); 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Chatbot Gunadarma',
+        layout: 'layouts/main-layout'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Gundar Chatbot | Listening at http://localhost:${port}`);
+});
